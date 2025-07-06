a4318b1f9aec90e51c1c195954fd5c44714cd1b9
Azazello <donazazello1312@gmail.com>
2025-07-06 16:32:19 +0300

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,14 @@
 function getElementWidth(content, padding, border) {
   return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
+
+function checkForSpam(message) {
+  if (
+    message.toLowerCase().includes("spam") ||
+    message.toLowerCase().includes("sale")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
