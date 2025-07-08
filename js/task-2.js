869f2c06c7df43f67031ba9aba25ba9a8eedb6c3
Azazello <donazazello1312@gmail.com>
2025-07-08 12:35:04 +0300

    del old func and add console

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,3 @@
-function getShippingMessage(country, price, deliveryFee) {
-  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
-}
-
 function formatMessage(message, maxLength) {
   let simbols = message.length;
   if (simbols <= maxLength) {
@@ -10,3 +6,9 @@ function formatMessage(message, maxLength) {
     return `${message.slice(0, maxLength)}...`;
   }
 }
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
